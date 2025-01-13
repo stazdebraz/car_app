988536ad44a26e98089101f3829c86e3bf3932fa
Dastan Toktosunov <48437157+stazdebraz@users.noreply.github.com>
2025-01-13 19:38:04 +0600

    feat/add welcome screen

---
+++ car_app/lib/core/resource/app_assets.dart
@@ -0,0 +1,4 @@
+abstract class AppAssets {
+  static const String welcomeBg = 'assets/welcome_bg.png';
+  static const String welcomeLogo = 'assets/welcome_logo.png';
+}
---
+++ car_app/lib/features/welcome/welcome_screen.dart
@@ -0,0 +1,77 @@
+import 'package:car_app/core/resource/app_assets.dart';
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image.asset(
+            AppAssets.welcomeBg,
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 300,
+                ),
+                SizedBox(
+                  height: 60,
+                  child: Image.asset(
+                    AppAssets.welcomeLogo,
+                  ),
+                ),
+                const SizedBox(
+                  height: 150,
+                ),
+                Text(
+                  'Rent your dream car from the',
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.w600,
+                    color: const Color(0xffF6F6F6).withOpacity(
+                      0.81,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Best Company',
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.w600,
+                    color: const Color(0xffF6F6F6).withOpacity(
+                      0.81,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xffC64949)),
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 14, horizontal: 65),
+                    child: Text(
+                      'Get Started',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
