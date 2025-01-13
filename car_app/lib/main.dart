8da074d2306b7e453c0b8c4344fbe0b487dcab35
Dastan Toktosunov <48437157+stazdebraz@users.noreply.github.com>
2025-01-13 18:53:45 +0600

    inti app

---
+++ car_app/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CarApp());
+}
+
+class CarApp extends StatelessWidget {
+  const CarApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
